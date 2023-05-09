84495d8f9a0f1bce07a908a753df6a4e567ee95d
RakeshRocky220 <kandhalarakeshreddy220.com>
2023-05-09 09:59:26 +0530

    Add homepage and scripts for GitHub Pages deployment

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import './App.css';
+import image from './assets/hacker.png'
+function App() {
+  return (
+   <>
+   
+   <div className='row mt-3'>
+  <div className='col-md-12 col-12 '>
+    <h2 style={{fontWeight:`bolder`}} className='text-dark text-center  font gradient-text1'>K Rakesh Reddy</h2>
+    <h6 className='text-dark text-center font gradient-text1'>kandhalarakesh007@gmail.com</h6>
+    <h6 className='text-dark text-center  font gradient-text1'>9110342725</h6>
+    <h6 className='text-dark text-center  font gradient-text1'>Sriramnagar,Suryapet,508213</h6>
+  </div>
+</div>
+
+
+
+<div class="row d-flex justify-content-center align-items-center">
+  <div class="col-12 col-md-auto text-center mb-2 mb-md-0">
+    <a href="https://www.linkedin.com/in/rakesh-reddy-53501222a/" class="btn btn-primary btn-sm">
+      <i class="fa fa-linkedin" aria-hidden="true"></i>&nbsp;Linkedin
+    </a>
+  </div>
+  <div class="col-12 col-md-auto text-center mb-2 mb-md-0">
+    <a href="https://github.com/RakeshRocky220" class="btn btn-danger btn-sm mx-2">
+      <i class="fa fa-github" aria-hidden="true"></i>&nbsp;Github
+    </a>
+  </div>
+  <div class="col-12 col-md-auto text-center">
+    <a href="https://www.hackerrank.com/kandhalarakeshr1" class="btn btn-success btn-sm">
+      <img src={image} style={{height: "25px", width: "25px"}} />&nbsp;HackerRank
+    </a>
+  </div>
+</div>
+
+
+   <br></br>
+   <h2 className="mx-2 text-dark font gradient-text" style={{fontWeight:`bolder`}}>
+  <i className="fa fa-graduation-cap" aria-hidden="true"></i>
+ &nbsp; <span className="gradient-text" >Education :</span>
+</h2>
+   <hr className='text-dark'></hr>
+
+  <div className='row mt-2'>
+      <div className='col-md-6'>
+        <h5 style={{fontWeight:`bolder`}} className='mx-2 text-dark font'>Malla Reddy Institute of Engineering and Technology</h5>
+        <h6 className='mx-2 text-dark font'>B Tech in Information Technology (2018-2022)</h6>
+        <h6 className='mx-2 text-dark font'>CGPA : 6.7</h6>
+        <br></br>
+        <h5 style={{fontWeight:`bolder`}} className='mx-2 text-dark font'>Narayana Junior College</h5>
+        <h6 className='mx-2 text-dark font'>Intermediate in MPC (2016-2018)</h6>
+        <h6 className='mx-2 text-dark font'>Percentage : 97%</h6>
+      </div>
+  </div>
+  
+    <br></br>
+   <h2 style={{fontWeight:`bolder`}} className='mx-2 text-dark gradient-text font'><i class="fa fa-certificate" aria-hidden="true"></i>&nbsp;&nbsp;<span className='gradient-text'>Certifications :</span></h2>
+   <hr className='text-dark'></hr>
+
+   <div className='row'>
+      <div className='col-md-6'>
+        <h6 className='text-dark mx-2'>- Problem Solving ( Basic ) - <b>HackerRank</b></h6>
+        <h6 className='text-dark mx-2'>- Java ( Basic ) - <b>HackerRank</b></h6>
+        <h6 className='text-dark mx-2'>- SQL ( Basic ) - <b>HackerRank</b></h6>
+        <h6 className='text-dark mx-2'>- Java Script ( Basic ) - <b>HackerRank</b></h6>
+        <a href='https://www.hackerrank.com/certificates/cbfca2a02191'><button className='btn btn-outline-success mx-3 mt-2'>View Credentials</button></a>
+      </div>
+   </div>
+
+   <br></br>
+   <h2 style={{fontWeight:`bolder`}} className='mx-2 text-dark gradient-text font'>&nbsp;<span className='gradient-text '>Skills :</span></h2>
+   <hr  className='text-dark'></hr>
+   <div className='container-fluid'>
+   <h4 style={{fontWeight:`bolder`}} className='mx-2 text-dark gradient-text font' ><span className='gradient-text'>Programming Launguages</span></h4>
+  
+   <div className='row d-flex flex-wrap  p-3'>
+  <div className='col-6 col-md-auto text-center'>
+    <img src='https://img.icons8.com/?size=512&id=20909&format=png' style={{height:`100px`,width:`100px`}} />
+    <p className='text-dark'>HTML5</p>
+  </div>
+  <div className='col-6 col-md-auto text-center'>
+    <img src='https://img.icons8.com/?size=512&id=7gdY5qNXaKC0&format=png' style={{height:`100px`,width:`100px`}} />
+    <p className='text-dark'>CSS3</p>
+  </div>
+  <div className='col-6 col-md-auto text-center'>
+    <img src='https://img.icons8.com/?size=512&id=108784&format=png' style={{height:`100px`,width:`100px`}} />
+    <p className='text-dark'>Java Script</p>
+  </div>
+  <div className='col-6 col-md-auto text-center'>
+    <img src='https://img.icons8.com/?size=512&id=13679&format=png' style={{height:`100px`,width:`100px`}} />
+    <p className='text-dark'>Java</p>
+  </div>
+</div>
+
+
+   <h4 style={{fontWeight:`bolder`}} className='mx-2 text-dark font' ><span className='gradient-text'>Libraries/Frameworks</span></h4>
+
+   <div className='row d-flex flex-wrap p-3'>
+        <div className='col-md-auto col-6 text-center p-1'>
+            <img className='mx-2' src='https://img.icons8.com/?size=512&id=Nlsua06Gvxel&format=png' style={{height:`100px`,width:`100px`}}></img>
+            <p className='text-center text-dark' >React</p>
+        </div>
+        <div className='col-md-auto col-6 text-center'>
+            <img  src='https://img.icons8.com/?size=512&id=90519&format=png' style={{height:`100px`,width:`100px`}}></img>
+            <p className='text-center text-dark' >Spring Boot</p>
+        </div>
+        <div className='col-md-auto col-6 text-center'>
+            <img  src='https://dwglogo.com/wp-content/uploads/2017/12/1100px_Hibernate_logo_01.png' style={{height:`100px`,width:`100px`}}></img>
+            <p className='text-center text-dark' >Hibernate</p>
+        </div>
+        <div className='col-md-auto col-6 text-center'>
+            <img src='https://img.icons8.com/?size=512&id=84710&format=png' style={{height:`100px`,width:`100px`}}></img>
+            <p className='text-center text-dark' >Bootstrap</p>
+        </div>
+
+   </div>
+
+   <h4 style={{fontWeight:`bolder`}} className='mx-2 text-dark font' ><span className='gradient-text'>Tools</span></h4>
+    <div className='row d-flex flex-wrap p-3'>
+      <div className='col-md-auto col-6 text-center'>
+        <img  src='https://img.icons8.com/?size=512&id=AZOZNnY73haj&format=png' style={{height:`100px`,width:`100px`}}></img>
+        <p className='text-center text-dark'>GitHub</p>
+      </div>
+      <div className='col-md-auto col-6 text-center'>
+        <img src='https://rdi.bright-research.com/wp-content/uploads/2020/10/postman.png' style={{height:`100px`,width:`100px`}}></img>
+        <p className='text-center text-dark'>PostMan</p>
+      </div>
+      <div className='col-md-auto col-6 text-center'>
+        <img src='https://img.icons8.com/?size=512&id=20906&format=png' style={{height:`100px`,width:`100px`}}></img>
+        <p className='text-center text-dark'>Git</p>
+      </div>
+    </div>
+
+
+    
+   <h4 style={{fontWeight:`bolder`}} className='mx-2 text-dark font' ><span className='gradient-text'>IDE's</span></h4>
+    <div className='row d-flex flex-wrap m-3'>
+      <div className='col-md-auto col-6 text-center'>
+        <img  className='mx-2' src='https://logodix.com/logo/732150.png' style={{height:`85px`,width:`85px`}}></img>
+        <p className='text-center text-dark'>Eclipse</p>
+      </div>
+      <div className='col-md-auto col-6 text-center'>
+        <img className='mx-2' src='https://1.bp.blogspot.com/-QsZYakuFeZc/YOu85v4QZCI/AAAAAAAAL4o/swwm0PB0uW8Y2-SEYLi--s8byFieqcshACNcBGAsYHQ/w400-h400/spring.webp' style={{height:`85px`,width:`85px`}}></img>
+        <p className='text-center text-dark'>STS</p>
+      </div>
+      <div className='col-md-auto col-6 text-center'>
+        <img  className='mx-2'src='https://img.icons8.com/?size=512&id=y7WGoWNuIWac&format=png' style={{height:`85px`,width:`85px`}}></img>
+        <p className='text-center text-dark'>VsCode</p>
+      </div>
+    </div>
+    </div>
+   </>
+  );
+}
+
+export default App;
